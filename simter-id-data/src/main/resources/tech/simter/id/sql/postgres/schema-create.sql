dbd9c514ee373842f6b4d28ca89bc539b08b2853
RJ.HWang <rongjihuang@gmail.com>
2017-08-11 17:38:11 +0800

    Add postgres sql

---
+++ simter-id-data/src/main/resources/tech/simter/id/sql/postgres/schema-create.sql
@@ -0,0 +1,7 @@
+create table id_holder (
+  type  varchar(100) primary key,
+  value bigint not null
+);
+comment on table id_holder is 'The ID Holder';
+comment on column id_holder.type is 'The type';
+comment on column id_holder.value is 'The current value';
