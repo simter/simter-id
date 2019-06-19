f8698c1c82ce176ecad76da9c94cd3dd18bbf845
RJ <rongjihuang@gmail.com>
2019-06-19 14:56:20 +0800

    TODO - concurrency invoke failed

--- simter-id-dao-mongo/src/test/kotlin/tech/simter/id/impl/dao/mongo/IdDaoImplTest.kt
+++ simter-id-dao-mongo/src/test/kotlin/tech/simter/id/impl/dao/mongo/IdDaoImplTest.kt
@@ -1,9 +1,12 @@
 package tech.simter.id.impl.dao.mongo
 
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig
+import reactor.core.publisher.Flux
+import reactor.core.scheduler.Schedulers
 import reactor.kotlin.test.test
 import tech.simter.id.core.IdDao
 import tech.simter.id.impl.dao.mongo.po.IdHolderPo
@@ -33,5 +36,28 @@ class IdDaoImplTest @Autowired constructor(
 
     // invoke and verify
     dao.nextLong(expected.t).test().expectNext(expected.v + 1L).verifyComplete()
+  }
+
+  @Test
+  fun `concurrency test`() {
+    val count = 2
+    val idType = randomString()
+    Flux.range(0, count)
+      .flatMap { dao.nextLong(idType) }
+      .parallel().runOn(Schedulers.parallel()).sequential()
+      .collectList()
+      .test()
+      .consumeNextWith {
+        // verify all id should not same
+        Assertions.assertEquals(count, it.size)
+        Assertions.assertEquals(count, it.toSet().size)
+
+        // verify should step 1
+        val sorted = it.toSet().sorted()
+        sorted.forEachIndexed { index, v ->
+          if (index < count - 1) Assertions.assertEquals(v + 1, sorted[index + 1])
+        }
+      }
+      .verifyComplete()
   }
 }
