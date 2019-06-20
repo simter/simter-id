2b0d52255b1cba62b7b723a7e20734f7b84a2b61
RJ <rongjihuang@gmail.com>
2019-06-21 00:07:37 +0800

    TODO

--- simter-id-dao-mongo/src/main/kotlin/tech/simter/id/impl/dao/mongo/IdDaoImpl.kt
+++ simter-id-dao-mongo/src/main/kotlin/tech/simter/id/impl/dao/mongo/IdDaoImpl.kt
@@ -1,7 +1,14 @@
 package tech.simter.id.impl.dao.mongo
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.mongodb.MongoTransactionManager
+import org.springframework.data.mongodb.ReactiveMongoTransactionManager
+import org.springframework.data.mongodb.core.ReactiveMongoOperations
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.ReactiveTransactionManager
+import org.springframework.transaction.annotation.Isolation
+import org.springframework.transaction.annotation.Transactional
+import org.springframework.transaction.reactive.TransactionalOperator
 import reactor.core.publisher.Mono
 import tech.simter.id.core.IdDao
 import tech.simter.id.impl.dao.mongo.po.IdHolderPo
@@ -13,9 +20,16 @@ import tech.simter.id.impl.dao.mongo.po.IdHolderPo
  */
 @Repository
 class IdDaoImpl @Autowired constructor(
+  //private val operations: ReactiveMongoOperations,
   private val repository: IdReactiveRepository
 ) : IdDao {
+  @Transactional(isolation = Isolation.SERIALIZABLE)
   override fun nextLong(t: String): Mono<Long> {
+    val a: ReactiveTransactionManager
+    val c: MongoTransactionManager
+    val e: ReactiveMongoTransactionManager
+    val operations: ReactiveMongoOperations
+    val b: TransactionalOperator
     // TODO lock
     return repository.findById(t)
       // plus 1
